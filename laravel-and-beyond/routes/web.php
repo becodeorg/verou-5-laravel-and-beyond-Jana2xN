f7c705f1cefc218fcbd4698eea64927108476077
Jana2xN <jana.neirinck@hotmail.com>
2024-02-08 13:56:06 +0100

    Add use App\Http\Controllers\UserController; to web.php

--- laravel-and-beyond/routes/web.php
+++ laravel-and-beyond/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
